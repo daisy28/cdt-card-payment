e724682f336a7e0b43517e36409fbb347d28e147
Gloria <ubahgloria16@gmail.com>
2023-09-01 16:13:41 +0100

    Feat: Added the year and month option

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 const cardContainer = document.querySelector(".card_container");
 const form = document.querySelector(".payment_form");
 const cardNumberInput = document.querySelector(".card_holder_number");
+const cardNumberInputs = document.querySelectorAll(".form_inputs input");
+const year = document.querySelector(".year");
+const month = document.querySelector(".month");
 
+// console.log(cardNumberInputs)
+cardNumberInputs.forEach(input => {
+     console.log(input.value)
+     input.addEventListener("keydown", () => {
+          console.log(input.value.length)
+     });
+
+     
+//      if (input.value.length > 4) {
+// console.log(input.value.length)
+               
+//                input.addEventListener("keydown", () => {
+//                     console.log("test")
+//                })
+//           }
+});
 
 form.addEventListener("submit", (e) => {
      e.preventDefault();
      formValidation();
+     cardNumberInput.value = null
 });
 
+
+
 const formValidation = () => {
-     console.log(cardNumberInput.value)
-     console.log(cardNumberInput.value[0])
-     console.log(cardNumberInput.value[1])
-     if (cardNumberInput.value[0] === 2 || 5) {
+     // cardNumberInputs.forEach(input => {
+     //      console.log(input.value)
+     //      if (input.value.length > 4) {
+     //           input.addEventListener("keydown", () => {
+     //                console.log("test")
+     //           })
+     //      }
+     // });
+     if (cardNumberInput.value[0] === "2") {
           cardContainer.innerHTML = cardUI("Mastercard");
-     } else if (cardNumberInput.value[0] === 4) {
-          cardContainer.innerHTML = cardUI("Visa");
-     } else if (cardNumberInput.value[0] === 3) {
+     } else if (cardNumberInput.value[0] === "3") {
           cardContainer.innerHTML = cardUI("American Express");
+     } else if (cardNumberInput.value[0] === "4") {
+          cardContainer.innerHTML = cardUI("Visa");
+     } else if (cardNumberInput.value[0] === "5") {
+          cardContainer.innerHTML = cardUI("Mastercard");
      } else {
           cardContainer.innerHTML = cardUI("Verve");
      }
