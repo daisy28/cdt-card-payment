6f2d3885ed37940da705fb4205230ecea81528ad
Gloria <ubahgloria16@gmail.com>
2023-09-04 20:36:29 +0100

    Minor fix

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ const cvvInput = document.querySelector("#cvv");
 const alert = document.querySelector(".alert_box");
 
 // expiry month and year
-let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"];
+let months = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEPT",
+  "OCT",
+  "NOV",
+  "DEC",
+];
 let selectMonth = new Date().getUTCMonth();
 let monthOption = `<option value="">Month</option>`;
 let yearStart = 2024;
@@ -20,114 +33,115 @@ let cardExpiryYear = new Date("2034").getFullYear();
 let yearOption = `<option value="">Year</option>`;
 
 // expiry month
-for (let i = 0; i < months.length; i++){
-     monthOption += `<option value="${months[i]}">${months[i]}</option>`
+for (let i = 0; i < months.length; i++) {
+  monthOption += `<option value="${months[i]}">${months[i]}</option>`;
 }
 month.innerHTML = monthOption;
 
 // expiry year
-for (let i = yearStart; i < cardExpiryYear; i++){
-     yearOption += `<option value="${i}">${i}</option>`
+for (let i = yearStart; i < cardExpiryYear; i++) {
+  yearOption += `<option value="${i}">${i}</option>`;
 }
 year.innerHTML = yearOption;
 
+// update the dom for month and year
 month.addEventListener("change", () => {
-     cardExpirationMonth.innerHTML = `<span>${month.value}/</span>`;
+  cardExpirationMonth.innerHTML = `<span>${month.value}/</span>`;
 });
 year.addEventListener("change", () => {
-     cardExpirationYear.innerHTML = `<span>${year.value}</span>`;
+  cardExpirationYear.innerHTML = `<span>${year.value}</span>`;
 });
-
 
 // Form validation
 form.addEventListener("keyup", () => {
-     // automatically move curosr/focus to next input element
-     cardNumberInputs.forEach(input => {
-          input.addEventListener("keydown", () => {
-               if (input.value.length > input.maxLength) {
-                    input.nextElementSibling.focus();
-               } else if (input.value.length < input.maxLength) {
-          input.style.borderColor = `crimson`;
-               } else {
-          input.style.borderColor = `green`;
-               };
-          });
-     });
-
-     // Validate card issuer
-     if (cardNumberInputs[0].value[0] === "2") {
-          cardIssuer.innerHTML = "Mastercard";
-     } else if (cardNumberInputs[0].value[0] === "3") {
-          cardIssuer.innerHTML = "American Express";
-     } else if (cardNumberInputs[0].value[0] === "4") {
-          cardIssuer.innerHTML = "Visa";
-     } else if (cardNumberInputs[0].value[0] === "5") {
-          cardIssuer.innerHTML = "Mastercard";
-     } else {
-          cardIssuer.innerHTML = "Verve";
-     }
-
-     if (cardNumberInputs[3].value.length > 3) {
-          cardNumberInputs[3].blur();
-     };
-
-     if (cardHolderNameInput.value.length < 1) {
-          cardHolderNameInput.style.borderColor = `crimson`;
-     } else {
-          cardHolderNameInput.style.borderColor = `green`;
-     }
      
-     // update dom with card details
-     let value = ``;
-     cardNumberInputs.forEach(input => {
-          value += `<span>${input.value}</span>`;
-     });
-     cardNumber.innerHTML = value;
-     cardHolderName.innerHTML = cardHolderNameInput.value;
+  // automatically move curosr/focus to next input element
+  cardNumberInputs.forEach((input) => {
+    input.addEventListener("keydown", () => {
+      if (input.value.length > input.maxLength) {
+        input.nextElementSibling.focus();
+      } else if (input.value.length < input.maxLength) {
+        input.style.borderColor = `crimson`;
+      } else {
+        input.style.borderColor = `green`;
+      }
+    });
+  });
 
-     if (cvvInput.value.length < 3) {
-          cvvInput.style.borderColor = `crimson`;
-     } else if (cvvInput.value.length > 4) {
-          cvvInput.style.borderColor = `crimson`;
-     } else {
-          cvvInput.style.borderColor = `green`;
-     }
+  // Validate card issuer
+  if (cardNumberInputs[0].value[0] === "2") {
+    cardIssuer.innerHTML = "Mastercard";
+  } else if (cardNumberInputs[0].value[0] === "3") {
+    cardIssuer.innerHTML = "American Express";
+  } else if (cardNumberInputs[0].value[0] === "4") {
+    cardIssuer.innerHTML = "Visa";
+  } else if (cardNumberInputs[0].value[0] === "5") {
+    cardIssuer.innerHTML = "Mastercard";
+  } else {
+    cardIssuer.innerHTML = "Verve";
+  }
+
+  if (cardNumberInputs[3].value.length > 3) {
+    cardNumberInputs[3].blur();
+  }
+
+  if (cardHolderNameInput.value.length < 1) {
+    cardHolderNameInput.style.borderColor = `crimson`;
+  } else {
+    cardHolderNameInput.style.borderColor = `green`;
+  }
+
+  // update dom with card details
+  let value = ``;
+  cardNumberInputs.forEach((input) => {
+    value += `<span>${input.value}</span>`;
+  });
+  cardNumber.innerHTML = value;
+  cardHolderName.innerHTML = cardHolderNameInput.value;
+
+  if (cvvInput.value.length < 3) {
+    cvvInput.style.borderColor = `crimson`;
+  } else if (cvvInput.value.length > 4) {
+    cvvInput.style.borderColor = `crimson`;
+  } else {
+    cvvInput.style.borderColor = `green`;
+  }
 });
 
-form.addEventListener("submit", e => {
-     e.preventDefault();
-const inputs = form.querySelectorAll("input");
-     inputs.forEach(input => {
-          if (input.value === "") {
-               input.style.borderColor = `crimson`;
-               alert.style.display = "block";
-               alert.style.color = "crimson";
-               alert.innerHTML = `
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputs = form.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value === "") {
+      input.style.borderColor = `crimson`;
+      alert.style.display = "block";
+      alert.style.color = "crimson";
+      alert.innerHTML = `
       <p>Your details are incorrect!</p>`;
-          } else if (cvvInput.value.length < 3) {
-               alert.style.color = "crimson";
-               alert.innerHTML = `
+    } else if (cvvInput.value.length < 3) {
+      alert.style.color = "crimson";
+      alert.innerHTML = `
       <p>Your details are incorrect!</p>`;
-          } else if (cvvInput.value.length > 4) {
-               alert.style.color = "crimson";
-               alert.innerHTML = `
+    } else if (cvvInput.value.length > 4) {
+      alert.style.color = "crimson";
+      alert.innerHTML = `
       <p>Your details are incorrect!</p>`;
-          } else if (month.value === "") {
-               month.style.borderColor = "crimson";
-               alert.innerHTML = `
+    } else if (month.value === "") {
+      month.style.borderColor = "crimson";
+      alert.innerHTML = `
       <p>Your details are incorrect!</p>`;
-          } else if (year.value === "") {
-               year.style.borderColor = "crimson";
-               alert.innerHTML = `
+    } else if (year.value === "") {
+      year.style.borderColor = "crimson";
+      alert.innerHTML = `
       <p>Your details are incorrect!</p>`;
-          } else {
-               input.style.borderColor = `green`;
-               month.style.borderColor = "green";
-               year.style.borderColor = "green";
-               alert.style.display = "block";
-               alert.style.color = "green";
-               alert.innerHTML = `
+    } else {
+      input.style.borderColor = `green`;
+      month.style.borderColor = "green";
+      year.style.borderColor = "green";
+      alert.style.display = "block";
+      alert.style.color = "green";
+      alert.innerHTML = `
       <p>Your payment was successful!</p>`;
-          }
-     });
-})
+    }
+  });
+});
