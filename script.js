bc6a085bc9a58be84e9caa042fd166a9cf3bbbbb
Gloria <ubahgloria16@gmail.com>
2023-09-04 15:13:26 +0100

    Bug fix: Fixed the dom for card number

--- script.js
+++ script.js
@@ -1,75 +1,53 @@
-const cardContainer = document.querySelector(".card_container");
+const cardIssuer = document.querySelector(".card_issuer");
+const cardNumber = document.querySelector(".cardholder_number");
 const form = document.querySelector(".payment_form");
-const cardNumberInput = document.querySelector(".card_holder_number");
 const cardNumberInputs = document.querySelectorAll(".form_inputs input");
+const cardHolderNameInput = document.querySelector("#card_holder");
+const cardHolderName = document.querySelector("#card_holder_name");
 const year = document.querySelector(".year");
 const month = document.querySelector(".month");
 
-// console.log(cardNumberInputs)
-cardNumberInputs.forEach(input => {
-     console.log(input.value)
-     input.addEventListener("keydown", () => {
-          console.log(input.value.length)
-     });
-
-     
-//      if (input.value.length > 4) {
-// console.log(input.value.length)
-               
-//                input.addEventListener("keydown", () => {
-//                     console.log("test")
-//                })
-//           }
-});
-
-form.addEventListener("submit", (e) => {
-     e.preventDefault();
-     formValidation();
-     cardNumberInput.value = null
-});
-
-
 
 const formValidation = () => {
-     // cardNumberInputs.forEach(input => {
-     //      console.log(input.value)
-     //      if (input.value.length > 4) {
-     //           input.addEventListener("keydown", () => {
-     //                console.log("test")
-     //           })
-     //      }
-     // });
-     if (cardNumberInput.value[0] === "2") {
-          cardContainer.innerHTML = cardUI("Mastercard");
-     } else if (cardNumberInput.value[0] === "3") {
-          cardContainer.innerHTML = cardUI("American Express");
-     } else if (cardNumberInput.value[0] === "4") {
-          cardContainer.innerHTML = cardUI("Visa");
-     } else if (cardNumberInput.value[0] === "5") {
-          cardContainer.innerHTML = cardUI("Mastercard");
+     cardNumberInputs.forEach(input => {
+     input.addEventListener("keydown", () => {
+          if (input.value.length > input.maxLength) {
+               input.nextElementSibling.focus();
+          };
+     });
+});
+
+     if (cardNumberInputs[0].value[0] === "2") {
+          cardIssuer.innerHTML = "Mastercard";
+     } else if (cardNumberInputs[0].value[0] === "3") {
+          cardIssuer.innerHTML = "American Express";
+     } else if (cardNumberInputs[0].value[0] === "4") {
+          cardIssuer.innerHTML = "Visa";
+     } else if (cardNumberInputs[0].value[0] === "5") {
+          cardIssuer.innerHTML = "Mastercard";
      } else {
-          cardContainer.innerHTML = cardUI("Verve");
+          cardIssuer.innerHTML = "Verve";
      }
+     if (cardNumberInputs[3].value.length > 3) {
+          cardNumberInputs[3].blur();
+     };
+
+     let value = ``;
+     cardNumberInputs.forEach(input => {
+          value += `<span>${input.value}</span>`;
+     });
+     cardNumber.innerHTML = value;
+
+     cardHolderName.innerHTML = cardHolderNameInput.value;
+     
 }
 
-const cardUI = (cardIssuer, cardHolder, expiry) => {
-     return `
-     <div class="payment_card">
-                    <div class="card_details_one">
-                         <img src="/assets/20230829_130215.jpg" alt="card-badge" class="class_badge">
-                         <p>${cardIssuer}</p>
-                    </div>
-                    <div class="card_details_two">
-                         <div>
-                              <p class="card_owner_name">card holder</p>
-                              <p>gloria ubah</p>
-                         </div>
+form.addEventListener("keyup", () => {
+     formValidation();
+});
 
-                         <div>
-                              <p class="card_expiration">expires</p>
-                              <p>29/10/2025</p>
-                         </div>
-                    </div>
-               </div>
-     `
-}
+// let cardExpiryYear = new Date("2024");
+// console.log(cardExpiryYear.getFullYear());
+// for (let i = 0; i < 10; i++){
+//      console.log(cardExpiryYear, i)
+// }
